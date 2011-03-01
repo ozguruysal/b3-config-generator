33703c01b5561b2a3a6a666593ed579da96b7202
Freelander <admin@fps-gamer.net>
2011-03-02 00:22:44 +0200

    css 2.1 valid now

--- index.php
+++ index.php
@@ -444,6 +444,12 @@ include('inc_configmyb3.php');
         <img style="border:none;" src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
       </a>
     </span>
+	<span class="validator_logo_css">
+      <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
+      </a>
+    </span>
+
 </div>
 
 <!--Privacy statement content starts here -->
