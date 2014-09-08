7fd22d6eec0f03e6cbef66b6f4fcea98b5e408eb
FenixXx <danielepantaleone@icloud.com>
2014-09-08 17:58:24 +0200

    [CONFIG] updated game configuration files to work with B3 1.10 dev

--- data/chiv.php
+++ data/chiv.php
@@ -5,7 +5,7 @@
  * Author URI: http://www.bigbrotherbot.net
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as 
+ * it under the terms of the GNU General Public License, version 2, as
  * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
@@ -21,9 +21,9 @@
 /**
  * This file holds an array of data for common b3 settings for all games/parsers
  */
- 
+
 /*-----------------------------------------------------------------------------*
- * HF SETTINGS
+ * CHIV SETTINGS
  *-----------------------------------------------------------------------------*/
 $game_name = 'Chivalry Medieval Warfare';
 
@@ -55,10 +55,10 @@ $server = array (
         'default'    => $_SERVER['REMOTE_ADDR']
     ),
     'port' => array (
-            'formtype'   => 'text',
-            'title'      => _('Server Query Port:'),
-            'tooltip'    => _('Enter your game server\'s Steam Query port. (see SteamQueryPort in your Ravaged server config file)'),
-            'default'    => '27015'
+        'formtype'   => 'text',
+        'title'      => _('Server Query Port:'),
+        'tooltip'    => _('Enter your game server\'s query port.'),
+        'default'    => '27015'
     ),
     'rcon_ip' => array (
         'formtype'   => 'text',
@@ -67,17 +67,23 @@ $server = array (
         'default'    => '127.0.0.1'
     ),
     'rcon_port' => array (
-            'formtype'   => 'text',
-            'title'      => _('Rcon Port:'),
-            'tooltip'    => _("Enter your game server's rcon (admin) port. (see 'RConPort' in section '[AOC.AOCRCon]' of your PCServer-UDKGame.ini game server config file)"),
-            'default'    => '27960'
+        'formtype'   => 'text',
+        'title'      => _('Rcon Port:'),
+        'tooltip'    => _("Enter your game server's rcon (admin) port."),
+        'default'    => '27960'
     ),
     'rcon_password' => array (
-            'formtype'   => 'text',
-            'title'      => _('Rcon Password:'),
-            'tooltip'    => _('Enter your rcon password. (see AdminPassword in your game server config file)'),
-            'default'    => 'MYrCOnPaSS'
-    )
+        'formtype'   => 'text',
+        'title'      => _('Rcon Password:'),
+        'tooltip'    => _('Enter your server rcon password.'),
+        'default'    => 'MYrCOnPaSS'
+    ),
+    'max_line_length' => array(
+        'formtype'   => 'text',
+        'title'      => _('Max Line Length'),
+        'tooltip'    => _('When sending in-game messages, lines will have at most this number of characters.'),
+        'default'    => '80'
+    ),
 );
 
 
@@ -147,55 +153,67 @@ $messages = array (
  * Built-in Plugins
  *******************************************************************************/
 $builtin_plugins = array (
-    'censor'   => array (
-        'formtype'   => 'plugin',
-        'title'      => 'Censor',
-        'tooltip'    => _('Cencors bad words and bad names'),
-        'default'    => '@conf/plugin_censor.xml'
-    ),
-    'spamcontrol'   => array (
-        'formtype'   => 'plugin',
-        'title'      => 'Spam Control',
-        'tooltip'    => _('Warns players who spams the in game chat'),
-        'default'    => '@conf/plugin_spamcontrol.ini'
-    ),
     'admin'   => array (
         'formtype'   => 'plugin',
         'title'      => 'Admin',
         'tooltip'    => _('This plugin provides main B3 functionalities.'),
         'default'    => '@conf/plugin_admin.ini',
         'checked'    => True,
-        'disabled'   => True 
-    ),
-    'tk'   => array (
-        'formtype'   => 'plugin',
-        'title'      => 'Team Kill',
-        'tooltip'    => _('Punishes team killers'),
-        'default'    => '@conf/plugin_tk.ini'
-    ),
-    'stats'   => array (
-        'formtype'   => 'plugin',
-        'title'      => 'Stats',
-        'tooltip'    => _('Displays round based stats'),
-        'default'    => '@conf/plugin_stats.ini'
-    ),
-    'pingwatch'   => array (
-        'formtype'   => 'plugin',
-        'title'      => 'Ping Watch',
-        'tooltip'    => _('Notifies players with high ping'),
-        'default'    => '@conf/plugin_pingwatch.ini'
+        'disabled'   => True
     ),
     'adv'   => array (
         'formtype'   => 'plugin',
         'title'      => 'Advertise',
-        'tooltip'    => _('Manages your server messages'),
+        'tooltip'    => _('Manages your server messages.'),
         'default'    => '@conf/plugin_adv.xml'
+    ),
+    'censor'   => array (
+        'formtype'   => 'plugin',
+        'title'      => 'Censor',
+        'tooltip'    => _('Censor bad words and bad names.'),
+        'default'    => '@conf/plugin_censor.xml'
+    ),
+    'cmdmanager'   => array (
+        'formtype'   => 'plugin',
+        'title'      => 'Commands Manager',
+        'tooltip'    => _('Allows to modify command levels and aliases in-game.'),
+        'default'    => '@conf/plugin_cmdmanager.ini'
+    ),
+    'login'   => array (
+        'formtype'   => 'plugin',
+        'title'      => 'Login',
+        'tooltip'    => _('Enable password authentication for server admins.'),
+        'default'    => '@conf/plugin_login.ini'
+    ),
+    'pingwatch'   => array (
+        'formtype'   => 'plugin',
+        'title'      => 'Ping Watch',
+        'tooltip'    => _('Notifies players with high ping.'),
+        'default'    => '@conf/plugin_pingwatch.ini'
+    ),
+    'spamcontrol'   => array (
+        'formtype'   => 'plugin',
+        'title'      => 'Spam Control',
+        'tooltip'    => _('Warns players who spams the in game chat.'),
+        'default'    => '@conf/plugin_spamcontrol.ini'
+    ),
+    'stats'   => array (
+        'formtype'   => 'plugin',
+        'title'      => 'Stats',
+        'tooltip'    => _('Displays round based stats.'),
+        'default'    => '@conf/plugin_stats.ini'
     ),
     'status'   => array (
         'formtype'   => 'plugin',
         'title'      => 'Status',
         'tooltip'    => _('Writes server status to an xml file. This can be used by 3rd party tools to monitor server status.'),
         'default'    => '@conf/plugin_status.ini'
+    ),
+    'tk'   => array (
+        'formtype'   => 'plugin',
+        'title'      => 'Team Kill',
+        'tooltip'    => _('Punishes team killers'),
+        'default'    => '@conf/plugin_tk.ini'
     ),
     'welcome'   => array (
         'formtype'   => 'plugin',
@@ -212,22 +230,22 @@ $external_plugins = array (
     'banlist' => array (
         'formtype'   => 'plugin',
         'title'      => 'Banlist',
-        'tooltip'    => 'Allows easy sharing of cheater banlist between clans',
-        'default'    => '@b3/extplugins/conf/banlist.xml',
+        'tooltip'    => 'Allows easy sharing of cheater banlist between clans.',
+        'default'    => '@b3/extplugins/conf/plugin_banlist.xml',
         'url'        => 'http://forum.bigbrotherbot.net/downloads/?sa=downfile&amp;id=6'
     ),
     'chatlogger' => array (
         'formtype'   => 'plugin',
         'title'      => 'Chatlogger',
         'tooltip'    => 'This plugin logs to database and/or file all clients\' messages (chat, team chat, private chat).',
-        'default'    => '@b3/extplugins/conf/plugin_chatlogger.xml',
+        'default'    => '@b3/extplugins/conf/plugin_chatlogger.ini',
         'url'        => 'http://forum.bigbrotherbot.net/downloads/?sa=downfile&id=7'
     ),
     'xlrstats' => array (
         'formtype'   => 'plugin',
         'title'      => 'XLRstats',
         'tooltip'    => 'Records all player stats in your database.',
-        'default'    => '@b3/extplugins/conf/xlrstats.xml',
+        'default'    => '@b3/extplugins/conf/plugin_xlrstats.ini',
         'url'        => 'http://www.xlrstats.com'
     )
 );
--- data/common_settings.php
+++ data/common_settings.php
@@ -5,7 +5,7 @@
  * Author URI: http://www.bigbrotherbot.net
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as 
+ * it under the terms of the GNU General Public License, version 2, as
  * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
@@ -21,156 +21,157 @@
 /**
  * This file holds an array of data for common b3 settings for all games/parsers
  */
- 
- /*******************************************************************************
+
+/*******************************************************************************
  * B3 Settings
  *******************************************************************************/
 $b3 = array(
-        'bot_name' => array (
-            'formtype'   => 'text',
-            'title'      => _('Bot Name:'), 
-            'tooltip'    => _('Name of the bot.'), 
-            'default'    => 'b3',
-            'always_hide'=> true
-        ),
-        'bot_prefix' => array (
-            'formtype'   => 'text',
-            'title'      => _('Bot Prefix:'), 
-            'tooltip'    => _('Ingame messages are prefixed with this code, you can use colorcodes.'), 
-            'default'    => '^0(^2b3^0)^7:'
-        ),
-        'time_format' => array (
-            'formtype'   => 'text',
-            'title'      => _('Time Format:'), 
-            'tooltip'    => _('You can arrange the time format as specified in python\'s time directives.'), 
-            'default'    => '%I:%M%p %Z %m/%d/%y'
-        ),
-        'time_zone' => array (
-            'formtype'   => 'text',
-            'title'      => _('Time Zone:'), 
-            'tooltip'    => _('Set your server\'s timezone so all timebased lookups will show the proper time.'), 
-            'default'    => 'CET'
-        ),
-        'log_level' => array (
-            'formtype'   => 'select',
-            'title'      => _('Log Level:'), 
-            'tooltip'    => _('The amount of logging you want in your b3.log file. Lower numbers log more information.'), 
-            'default'    => 9,
-            'options'    => array (
-                8  => _('Extra verbose debugging'),
-                9  => _('Verbose debugging'),
-                10 => _('Debugging'),
-                20 => _('Extra information'),
-                21 => _('B3 log messages'),
-                22 => _('Game log messages'),
-                30 => _('Warnings'),
-                40 => _('Errors'),
-                50 => _('Critical Errors')
-            )
-        ),
-        'logfile' => array (
-            'formtype' => 'text',
-            'title'      => _('B3 Log File:'), 
-            'tooltip'    => _('Name and path of the logfile the bot will generate.<br/>@conf : the folder containing this config file'),
-            'default'    => '@conf/b3.log'
-        ),
+    'bot_name' => array (
+        'formtype'   => 'text',
+        'title'      => _('Bot Name:'),
+        'tooltip'    => _('Name of the bot.'),
+        'default'    => 'b3',
+        'always_hide'=> true
+    ),
+    'bot_prefix' => array (
+        'formtype'   => 'text',
+        'title'      => _('Bot Prefix:'),
+        'tooltip'    => _('Ingame messages are prefixed with this code: you can use colorcodes.'),
+        'default'    => '^0(^2b3^0)^7:'
+    ),
+    'time_format' => array (
+        'formtype'   => 'text',
+        'title'      => _('Time Format:'),
+        'tooltip'    => _('You can arrange the time format as specified in python\'s time directives.'),
+        'default'    => '%I:%M%p %Z %m/%d/%y'
+    ),
+    'time_zone' => array (
+        'formtype'   => 'text',
+        'title'      => _('Time Zone:'),
+        'tooltip'    => _('Set your server\'s timezone so all timebased lookups will show the proper time.'),
+        'default'    => 'CET'
+    ),
+    'log_level' => array (
+        'formtype'   => 'select',
+        'title'      => _('Log Level:'),
+        'tooltip'    => _('The amount of logging you want in your b3.log file. Lower numbers log more information.'),
+        'default'    => 9,
+        'options'    => array (
+            8  => _('Extra verbose debugging'),
+            9  => _('Verbose debugging'),
+            10 => _('Debugging'),
+            20 => _('Extra information'),
+            21 => _('B3 log messages'),
+            22 => _('Game log messages'),
+            30 => _('Warnings'),
+            40 => _('Errors'),
+            50 => _('Critical Errors')
+        )
+    ),
+    'logfile' => array (
+        'formtype' => 'text',
+        'title'      => _('B3 Log File:'),
+        'tooltip'    => _('Name and path of the logfile the bot will generate.<br/>@conf : the folder containing this config file'),
+        'default'    => '@conf/b3.log'
+    ),
 );
 
- /*******************************************************************************
+/*******************************************************************************
  * Database Settings
  *******************************************************************************/
 $database = array (
-        'db_user' => array (
-            'formtype'   => 'text',
-            'title'      => _('Username:'),
-            'tooltip'    => _('Your B3 database username.'),
-            'default'    => 'root'
-        ),
-        'db_pass' => array (
-            'formtype'   => 'text',
-            'title'      => _('Password:'),
-            'tooltip'    => _('Your B3 database password.'),
-            'default'    => 'mYPASswOrd'
-        ),
-        'db_host' => array (
-            'formtype'   => 'text',
-            'title'      => _('Hostname/IP:'),
-            'tooltip'    => _('Enter your database hostname or the IP number if your database is on a remote server. 
-                               But make sure the database server accepts your connection from outside, this is a common mistake.'),
-            'default'    => 'localhost'
-        ),
-        'db_name' => array (
-            'formtype'   => 'text',
-            'title'      => _('Database Name:'),
-            'tooltip'    => _('Your B3 database name.'),
-            'default'    => 'b3_db'
-        )
+    'db_user' => array (
+        'formtype'   => 'text',
+        'title'      => _('Username:'),
+        'tooltip'    => _('Your B3 database username.'),
+        'default'    => 'root'
+    ),
+    'db_pass' => array (
+        'formtype'   => 'text',
+        'title'      => _('Password:'),
+        'tooltip'    => _('Your B3 database password.'),
+        'default'    => 'mYPASswOrd'
+    ),
+    'db_host' => array (
+        'formtype'   => 'text',
+        'title'      => _('Hostname/IP:'),
+        'tooltip'    => _('Enter your database hostname or the IP number if your database is on a remote server.
+                           But make sure the database server accepts your connection from outside, this is a common mistake.'),
+        'default'    => 'localhost'
+    ),
+    'db_name' => array (
+        'formtype'   => 'text',
+        'title'      => _('Database Name:'),
+        'tooltip'    => _('Your B3 database name.'),
+        'default'    => 'b3_db'
+    )
 );
 
- /*******************************************************************************
+/*******************************************************************************
  * Autodoc Settings
  *******************************************************************************/
 $autodoc = array (
-        'type' => array (
-            'formtype'   => 'select',
-            'title'      => _('Type:'),
-            'tooltip'    => _('Select autodoc file type you want to use.'),
-            'default'    => 'html',
-            'options'    => array (
-                'html' => 'html',
-                'htmltable' => 'htmltable',
-                'xml' => 'xml'
-            )
-        ),
-        'maxlevel' => array (
-            'formtype'   => 'text',
-            'title'      => _('Max Level:'),
-            'tooltip'    => _('If you want to exclude commands reserved for higher levels, you may enter a lower value here.'),
-            'default'    => '100'
-        ),
-        'destination' => array (
-            'formtype'   => 'multioption',
-            'title'      => _('Destination:'),
-            'tooltip'    => _('Select the location where you want to generate your command documentation file.'),
-            'default'    => 'locala',
-            'options'    => array (
-                'locala' => array (
-                    'title'         => _('Local File'),
-                    'hide'          => 1, //Unique number for hide div
-                    'autodoc_local' => array (
-                        'formtype'   => 'text',
-                        'title'      => _('Local Path:'),
-                        'tooltip'    => _('Enter the full path and file name.<br/>@conf : the folder containing this config file'),
-                        'default'    => '@conf/b3_doc.html'
-                    )
+    'type' => array (
+        'formtype'   => 'select',
+        'title'      => _('Type:'),
+        'tooltip'    => _('Select autodoc file type you want to use.'),
+        'default'    => 'html',
+        'options'    => array (
+            'html' => 'html',
+            'htmltable' => 'htmltable',
+            'json' => 'json',
+            'xml' => 'xml'
+        )
+    ),
+    'maxlevel' => array (
+        'formtype'   => 'text',
+        'title'      => _('Max Level:'),
+        'tooltip'    => _('If you want to exclude commands reserved for higher levels, you may enter a lower value here.'),
+        'default'    => '100'
+    ),
+    'destination' => array (
+        'formtype'   => 'multioption',
+        'title'      => _('Destination:'),
+        'tooltip'    => _('Select the location where you want to generate your command documentation file.'),
+        'default'    => 'locala',
+        'options'    => array (
+            'locala' => array (
+                'title'         => _('Local File'),
+                'hide'          => 1, //Unique number for hide div
+                'autodoc_local' => array (
+                    'formtype'   => 'text',
+                    'title'      => _('Local Path:'),
+                    'tooltip'    => _('Enter the full path and file name.<br/>@conf : the folder containing this config file'),
+                    'default'    => '@conf/b3_doc.html'
+                )
+            ),
+            'ftpa' => array (
+                'title'          => _('FTP Location'),
+                'hide'           => 2, //Unique number for hide div
+                'autodoc_ftpadr' => array (
+                    'formtype'    => 'text',
+                    'title'       => _('FTP Address:'),
+                    'tooltip'     => _('Enter the FTP address for your B3 command documentation file.'),
+                    'default'     => 'ftp://ftp.example.com/games/b3_doc.html'
                 ),
-                'ftpa' => array (
-                    'title'          => _('FTP Location'),
-                    'hide'           => 2, //Unique number for hide div
-                    'autodoc_ftpadr' => array (
-                        'formtype'    => 'text',
-                        'title'       => _('FTP Address:'),
-                        'tooltip'     => _('Enter the FTP address for your B3 command documentation file.'),
-                        'default'     => 'ftp://ftp.example.com/games/b3_doc.html'
-                    ),
-                    'autodoc_ftpusr' => array (
-                        'formtype'   => 'text',
-                        'title'      => _('FTP User:'),
-                        'tooltip'    => _('Enter your ftp user name'),
-                        'default'    => 'ftpuser'
-                    ),
-                    'autodoc_ftppas' => array (
-                        'formtype'   => 'text',
-                        'title'      => _('FTP Pass:'),
-                        'tooltip'    => _('Enter your ftp password'),
-                        'default'    => 'FtPpASSw0Rd'
-                    )
+                'autodoc_ftpusr' => array (
+                    'formtype'   => 'text',
+                    'title'      => _('FTP User:'),
+                    'tooltip'    => _('Enter your ftp user name'),
+                    'default'    => 'ftpuser'
+                ),
+                'autodoc_ftppas' => array (
+                    'formtype'   => 'text',
+                    'title'      => _('FTP Pass:'),
+                    'tooltip'    => _('Enter your ftp password'),
+                    'default'    => 'FtPpASSw0Rd'
                 )
             )
         )
+    )
 );
 
- /*******************************************************************************
+/*******************************************************************************
  * Update
  *******************************************************************************/
 $update = array (
@@ -187,77 +188,76 @@ $update = array (
     )
 );
 
- /*******************************************************************************
+/*******************************************************************************
  * Messages
  *******************************************************************************/
 $messages = array (
     'kicked_by' => array (
-            'formtype'   => 'text',
-            'title'      => _('Kicked By:'),
-            'tooltip'    => _('The message that will be displayed when a player is kicked by a mod/admin.'),
-            'default'    => '$clientname^7 was kicked by $adminname^7 $reason',
-            'size'       => '420px'
+        'formtype'   => 'text',
+        'title'      => _('Kicked By:'),
+        'tooltip'    => _('The message that will be displayed when a player is kicked by a mod/admin.'),
+        'default'    => '$clientname^7 was kicked by $adminname^7 $reason',
+        'size'       => '420px'
     ),
     'kicked' => array (
-            'formtype'   => 'text',
-            'title'      => _('Kicked:'),
-            'tooltip'    => _('The message that will be displayed when a player is kicked by B3.'),
-            'default'    => '$clientname^7 was kicked $reason',
-            'size'       => '420px'
+        'formtype'   => 'text',
+        'title'      => _('Kicked:'),
+        'tooltip'    => _('The message that will be displayed when a player is kicked by B3.'),
+        'default'    => '$clientname^7 was kicked $reason',
+        'size'       => '420px'
     ),
     'banned_by' => array (
-            'formtype'   => 'text',
-            'title'      => _('Banned By:'),
-            'tooltip'    => _('The message that will be displayed when a player is banned by a mod/admin.'),
-            'default'    => '$clientname^7 was banned by $adminname^7 $reason',
-            'size'       => '420px'
+        'formtype'   => 'text',
+        'title'      => _('Banned By:'),
+        'tooltip'    => _('The message that will be displayed when a player is banned by a mod/admin.'),
+        'default'    => '$clientname^7 was banned by $adminname^7 $reason',
+        'size'       => '420px'
     ),
     'banned' => array (
-            'formtype'   => 'text',
-            'title'      => _('Banned:'),
-            'tooltip'    => _('The message that will be displayed when a player is kicked by a B3.'),
-            'default'    => '$clientname^7 was banned $reason',
-            'size'       => '420px'
+        'formtype'   => 'text',
+        'title'      => _('Banned:'),
+        'tooltip'    => _('The message that will be displayed when a player is kicked by a B3.'),
+        'default'    => '$clientname^7 was banned $reason',
+        'size'       => '420px'
     ),
     'temp_banned_by' => array (
-            'formtype'   => 'text',
-            'title'      => _('Temp Banned By:'),
-            'tooltip'    => _('The message that will be displayed when a player is temp banned by a mod/admin.'),
-            'default'    => '$clientname^7 was temp banned by $adminname^7 for $banduration^7 $reason',
-            'size'       => '420px'
+        'formtype'   => 'text',
+        'title'      => _('Temp Banned By:'),
+        'tooltip'    => _('The message that will be displayed when a player is temp banned by a mod/admin.'),
+        'default'    => '$clientname^7 was temp banned by $adminname^7 for $banduration^7 $reason',
+        'size'       => '420px'
     ),
     'temp_banned' => array (
-            'formtype'   => 'text',
-            'title'      => _('Temp Banned:'),
-            'tooltip'    => _('The message that will be displayed when a player is temp banned by B3.'),
-            'default'    => '$clientname^7 was temp banned for $banduration^7 $reason',
-            'size'       => '420px'
+        'formtype'   => 'text',
+        'title'      => _('Temp Banned:'),
+        'tooltip'    => _('The message that will be displayed when a player is temp banned by B3.'),
+        'default'    => '$clientname^7 was temp banned for $banduration^7 $reason',
+        'size'       => '420px'
     ),
     'unbanned_by' => array (
-            'formtype'   => 'text',
-            'title'      => _('Unbanned By:'),
-            'tooltip'    => _('The message that will be displayed when a player is unbanned by a mod/admin.'),
-            'default'    => '$clientname^7 was un-banned by $adminname^7 $reason',
-            'size'       => '420px'
+        'formtype'   => 'text',
+        'title'      => _('Unbanned By:'),
+        'tooltip'    => _('The message that will be displayed when a player is unbanned by a mod/admin.'),
+        'default'    => '$clientname^7 was un-banned by $adminname^7 $reason',
+        'size'       => '420px'
     ),
     'unbanned' => array (
-            'formtype'   => 'text',
-            'title'      => _('Unbanned:'),
-            'tooltip'    => _('The message that will be displayed when a player is unbanned by B3'),
-            'default'    => '$clientname^7 was un-banned $reason',
-            'size'       => '420px'
+        'formtype'   => 'text',
+        'title'      => _('Unbanned:'),
+        'tooltip'    => _('The message that will be displayed when a player is unbanned by B3'),
+        'default'    => '$clientname^7 was un-banned $reason',
+        'size'       => '420px'
     )
 );
- 
-  /*******************************************************************************
+
+/*******************************************************************************
  * External Plugins Directory
  *******************************************************************************/
  $external_plugins_dir = array (
     'external_dir' => array (
-            'formtype'   => 'text',
-            'title'      => _('External Directory:'),
-            'tooltip'    => _('Enter here the path to the config folder for your external plugins.'),
-            'default'    => '@b3/extplugins'
+        'formtype'   => 'text',
+        'title'      => _('External Directory:'),
+        'tooltip'    => _('Enter here the path to the config folder for your external plugins.'),
+        'default'    => '@b3/extplugins'
     )
 );
- 
